6f7208b909ffcad373ba52c6083ba18b4abf02cc
Michael Eby <michaeleby1@gmail.com>
2018-04-19 02:11:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,10 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  array = ['3', '5']
-  array.unshift('1')
+  [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
